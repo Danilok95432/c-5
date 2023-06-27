4907ee25d72700a41905964c480d6faa75a1b134
Kramskoy-v <kramskoy.va@gmail.com>
2023-06-27 17:39:04 +0300

    Стр booking.html стационарка и адаптив и selection-rooms.html стационарка

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -3,3 +3,13 @@ import {Swiper} from "swiper/bundle";
 
 // Инициализация слайдеров
 
+const roomsListSwiper = new Swiper('.rooms-list__swiper', {
+    grabCursor: true,
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 50,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+})
