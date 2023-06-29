ad7ef191bd83f6204f820a1f300762cd81838a02
Kramskoy-v <kramskoy.va@gmail.com>
2023-06-29 17:42:10 +0300

    Стр services.html стационарка и адаптив

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -24,3 +24,14 @@ const roomsListSwiper = new Swiper('.rooms-list__swiper', {
         clickable: true,
     },
 })
+
+const servicesSwiper = new Swiper('.services__swiper', {
+    grabCursor: true,
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+})
