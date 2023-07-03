155376290de9c2046b04a51b5f8c34c4fad51712
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-07-03 15:10:21 +0300

    адаптив guest-data.html

--- src/js/components/show-room.js
+++ src/js/components/show-room.js
@@ -1,9 +1,12 @@
 const listItems = document.querySelectorAll('.rooms-list__item')
 const btnShow = document.querySelector('.rooms-list__btn-show')
 
-btnShow.addEventListener('click', ()=>{
-    btnShow.classList.toggle('_active');
-    listItems.forEach(item=>{
-        item.classList.toggle('_show')
+if (btnShow && listItems) {
+  btnShow.addEventListener('click', () => {
+    btnShow.classList.toggle('_active')
+    listItems.forEach(item => {
+      item.classList.toggle('_show')
     })
-})
+  })
+}
+
