7cdf78b29e5177cd639eeeff6534f70be5f6e23f
Kramskoy-v <kramskoy.va@gmail.com>
2023-07-14 17:00:02 +0300

    Добавлено бургер меню для мобилки

--- src/js/components/fixedNavMenu.js
+++ src/js/components/fixedNavMenu.js
@@ -1,15 +1,30 @@
-
-const findResForm = document.querySelector('.find-res-form')
+const findResForm = document.querySelector(".find-res-form");
 
 if (findResForm) {
-  window.addEventListener('scroll', () => {
-    let formCoord = findResForm.getBoundingClientRect().y + 200
+  window.addEventListener("scroll", () => {
+    let formCoord = findResForm.getBoundingClientRect().y + 200;
 
     if (scrollY > formCoord) {
-      findResForm.classList.add('_fixed')
+      findResForm.classList.add("_fixed");
     } else {
-      findResForm.classList.remove('_fixed')
+      findResForm.classList.remove("_fixed");
     }
-  } )
+  });
 }
 
+// анимация бургера
+
+const burgerOpen = document.querySelector(".main-menu__burger-btn");
+const burgerClose = document.querySelector(".main-menu__nav-btn-close");
+const navigationList = document.querySelector(".main-menu__nav");
+
+if (burgerOpen) {
+  burgerOpen.addEventListener("click", () => {
+    navigationList.classList.add("_active");
+    document.body.style.overflow = "hidden";
+  });
+  burgerClose.addEventListener("click", () => {
+    navigationList.classList.remove("_active");
+    document.body.style.overflow = "";
+  });
+}
