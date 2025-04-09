5253396af4e80fc64e410efc7e838690e72811dd
Danilok95432 <danila.dvoretskov@yandexe.ru>
2025-04-09 16:19:19 +0300

    правка дат

--- src/js/components/bookingDates.js
+++ src/js/components/bookingDates.js
@@ -1,39 +1,48 @@
-import AirDatepicker from 'air-datepicker'
+import AirDatepicker from "air-datepicker";
 
-const bookingDatesWrapper = document.querySelector('.booking-dates')
+const bookingDatesWrapper = document.querySelector(".booking-dates");
 if (bookingDatesWrapper) {
-  const checkinInput = bookingDatesWrapper.querySelector('._checkin')
-  const checkoutInput = bookingDatesWrapper.querySelector('._checkout')
-
+  const checkinInput = bookingDatesWrapper.querySelector("._checkin");
+  const checkoutInput = bookingDatesWrapper.querySelector("._checkout");
 
   const setStartDate = (input) => {
-    const inputValue = input.value
-    return inputValue ? new Date(inputValue) : new Date()
-  }
+    const inputValue = input.value;
+    return inputValue ? new Date(inputValue) : new Date();
+  };
 
-  const checkoutDatepicker = new AirDatepicker(checkoutInput, {
-    selectedDates: [setStartDate(checkoutInput)],
-    minDate: new Date(),
-
-  })
-
-  new AirDatepicker(checkinInput, {
+  const checkinDatepicker = new AirDatepicker(checkinInput, {
     selectedDates: [setStartDate(checkinInput)],
     minDate: new Date(),
     onSelect: ({ date }) => {
-      if (checkoutDatepicker.selectedDates[0] < date) {
-        checkoutDatepicker.update({
-          minDate: date,
-          selectedDates: [date]
-        })
+      // Вычисляем дату для checkout (следующий день)
+      const nextDay = new Date(date);
+      nextDay.setDate(date.getDate() + 1);
+
+      // Обновляем параметры checkoutDatepicker
+      checkoutDatepicker.update({
+        minDate: nextDay,
+        selectedDates: [nextDay],
+      });
+    },
+  });
+
+  const checkoutDate = new Date(checkinDatepicker.selectedDates[0]);
+  checkoutDate.setDate(checkoutDate.getDate() + 1);
+  const checkoutDatepicker = new AirDatepicker(checkoutInput, {
+    selectedDates: [checkoutDate],
+    minDate: new Date(
+      checkinDatepicker.selectedDates[0].getTime() + 1000 * 60 * 60 * 24
+    ),
+    onSelect: ({ date }) => {
+      if (date <= checkinDatepicker.selectedDates[0]) {
+        checkoutDatepicker.clear();
       }
     },
-  })
+  });
 
-
-  const allDateInputs = [checkinInput, checkoutInput]
+  const allDateInputs = [checkinInput, checkoutInput];
   allDateInputs.forEach((dateInput) => {
-    dateInput.addEventListener('keydown', (e) => e.preventDefault())
-    dateInput.addEventListener('paste', (e) => e.preventDefault())
-  })
+    dateInput.addEventListener("keydown", (e) => e.preventDefault());
+    dateInput.addEventListener("paste", (e) => e.preventDefault());
+  });
 }
