c4caa64736376e3f6a7d4bf1779a077502a0e54a
Danilok95432 <danila.dvoretskov@yandexe.ru>
2025-04-11 15:12:43 +0300

    правки дат выезда

--- src/js/components/bookingDates.js
+++ src/js/components/bookingDates.js
@@ -1,48 +1,59 @@
-import AirDatepicker from "air-datepicker";
+import AirDatepicker from "air-datepicker"
 
-const bookingDatesWrapper = document.querySelector(".booking-dates");
+const bookingDatesWrapper = document.querySelector(".booking-dates")
 if (bookingDatesWrapper) {
-  const checkinInput = bookingDatesWrapper.querySelector("._checkin");
-  const checkoutInput = bookingDatesWrapper.querySelector("._checkout");
+  const checkinInput = bookingDatesWrapper.querySelector("._checkin")
+  const checkoutInput = bookingDatesWrapper.querySelector("._checkout")
 
   const setStartDate = (input) => {
-    const inputValue = input.value;
-    return inputValue ? new Date(inputValue) : new Date();
-  };
+    const inputValue = input.value
+    return inputValue ? new Date(inputValue) : new Date()
+  }
 
   const checkinDatepicker = new AirDatepicker(checkinInput, {
     selectedDates: [setStartDate(checkinInput)],
     minDate: new Date(),
     onSelect: ({ date }) => {
-      // Вычисляем дату для checkout (следующий день)
-      const nextDay = new Date(date);
-      nextDay.setDate(date.getDate() + 1);
+      const nextDay = new Date(date)
+      nextDay.setDate(date.getDate() + 1)
 
-      // Обновляем параметры checkoutDatepicker
+      const maxCheckoutDate = new Date(date)
+      maxCheckoutDate.setFullYear(date.getFullYear() + 1)
+      maxCheckoutDate.setDate(date.getDate() - 1)
+
       checkoutDatepicker.update({
         minDate: nextDay,
+        maxDate: maxCheckoutDate, 
         selectedDates: [nextDay],
-      });
+      })
     },
-  });
+  })
 
-  const checkoutDate = new Date(checkinDatepicker.selectedDates[0]);
-  checkoutDate.setDate(checkoutDate.getDate() + 1);
+  const checkoutDate = new Date(checkinDatepicker.selectedDates[0])
+  checkoutDate.setDate(checkoutDate.getDate() + 1)
+
+  const initialMaxCheckoutDate = new Date(checkinDatepicker.selectedDates[0])
+  initialMaxCheckoutDate.setFullYear(
+    checkinDatepicker.selectedDates[0].getFullYear() + 1
+  )
+  initialMaxCheckoutDate.setDate(checkinDatepicker.selectedDates[0].getDate() - 1)
+
   const checkoutDatepicker = new AirDatepicker(checkoutInput, {
     selectedDates: [checkoutDate],
     minDate: new Date(
       checkinDatepicker.selectedDates[0].getTime() + 1000 * 60 * 60 * 24
     ),
+    maxDate: initialMaxCheckoutDate,
     onSelect: ({ date }) => {
       if (date <= checkinDatepicker.selectedDates[0]) {
-        checkoutDatepicker.clear();
+        checkoutDatepicker.clear()
       }
     },
-  });
+  })
 
-  const allDateInputs = [checkinInput, checkoutInput];
+  const allDateInputs = [checkinInput, checkoutInput]
   allDateInputs.forEach((dateInput) => {
-    dateInput.addEventListener("keydown", (e) => e.preventDefault());
-    dateInput.addEventListener("paste", (e) => e.preventDefault());
-  });
+    dateInput.addEventListener("keydown", (e) => e.preventDefault())
+    dateInput.addEventListener("paste", (e) => e.preventDefault())
+  })
 }
